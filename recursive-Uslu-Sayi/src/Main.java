7bb58723badd915b107055b46eaa5f8da58850d5
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2023-12-19 17:31:26 +0300

    recursive uslu sayı yapıldı

---
+++ recursive-Uslu-Sayi/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void power(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü Alınacak sayıyı giriniz : ");
+        int a = input.nextInt();
+
+        System.out.print("Üssü giriniz : ");
+        int b = input.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= b; i++){
+            result *= a;
+        }
+
+        System.out.println("Sonuç : " + result);
+
+    }
+
+    public static void main(String[] args) {
+        power();
+        }
+    }
