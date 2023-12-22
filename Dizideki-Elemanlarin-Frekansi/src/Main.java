8d2a302cdc539effb993a284f0e9ca96801fa1ac
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2023-12-22 17:06:41 +0300

    dizideki elemanlar kaç kere tekrar ediyor yapıldı.

---
+++ Dizideki-Elemanlarin-Frekansi/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int sayac = 0;
+            while (i < arr.length - 1 && arr[i] == arr[i + 1]) {
+                sayac++;
+                i++;
+            }
+            sayac++;
+            System.out.println(arr[i] + " Sayısı " + sayac + " kere tekrar edildi");
+        }
+
+    }
+}
